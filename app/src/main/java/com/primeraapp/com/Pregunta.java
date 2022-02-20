09957fc3eddfe9f79ed87fcc1c1964facbbc34f4
tortmac1 <tortmacdelaletra@gmail.com>
2022-02-20 22:15:36 +0100

    Esta version funciona con el emulador de movil, es la primera version estable

---
+++ app/src/main/java/com/primeraapp/com/Pregunta.java
@@ -0,0 +1,19 @@
+package com.primeraapp.com;
+
+public class Pregunta {
+    int id;
+    String texto;
+    boolean respuesta;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public boolean isRespuesta() {
+        return respuesta;
+    }
+}
